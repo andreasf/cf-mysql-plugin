26d607a9e157003c078633179bab1c038ff8625f
Andreas Fleig <afleig@pivotal.io>
2016-12-01 11:25:22 +0100

    Bump version to 1.3.1 - parallel API calls
    [finishes #134578059]

--- cfmysql/plugin.go
+++ cfmysql/plugin.go
@@ -24,7 +24,7 @@ func (self *MysqlPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 1,
 			Minor: 3,
-			Build: 0,
+			Build: 1,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
