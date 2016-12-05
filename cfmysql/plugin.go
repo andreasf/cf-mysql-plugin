a620ae5e95b376ea1e52fbe1cc5512f84ad36ce9
Andreas Fleig <afleig@pivotal.io>
2016-12-05 10:14:02 +0100

    Bump version to 1.3.4
    Shows services from current space only

--- cfmysql/plugin.go
+++ cfmysql/plugin.go
@@ -24,7 +24,7 @@ func (self *MysqlPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 1,
 			Minor: 3,
-			Build: 3,
+			Build: 4,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
