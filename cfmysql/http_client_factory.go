5057b857f6fbcd409ed6803c95323e980729ad04
Yitao Jiang <jiangyt.cn@gmail.com>
2017-09-12 11:54:48 +0800

    Read the proxy environment variable to call service api

--- cfmysql/http_client_factory.go
+++ cfmysql/http_client_factory.go
@@ -1,8 +1,8 @@
 package cfmysql
 
 import (
-	"net/http"
 	"crypto/tls"
+	"net/http"
 )
 
 //go:generate counterfeiter . HttpClientFactory
@@ -14,10 +14,11 @@ func NewHttpClientFactory() HttpClientFactory {
 	return new(httpClientFactory)
 }
 
-type httpClientFactory struct {}
+type httpClientFactory struct{}
 
 func (self *httpClientFactory) NewClient(sslDisabled bool) *http.Client {
 	transport := &http.Transport{
+		Proxy:           http.ProxyFromEnvironment,
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: sslDisabled},
 	}
 	return &http.Client{Transport: transport}
