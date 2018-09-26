c48b1374d21f11f47441bbec3de25e5a9e2d7f65
Andreas Fleig <afleig@pivotal.io>
2018-09-26 16:58:57 +0200

    go fmt
    [#160794441]

--- cfmysql/ioutil_wrapper.go
+++ cfmysql/ioutil_wrapper.go
@@ -14,7 +14,7 @@ func NewIoUtilWrapper() IoUtilWrapper {
 	return new(ioUtilWrapper)
 }
 
-type ioUtilWrapper struct {}
+type ioUtilWrapper struct{}
 
 func (self *ioUtilWrapper) TempFile(dir, pattern string) (f *os.File, err error) {
 	return ioutil.TempFile(dir, pattern)
